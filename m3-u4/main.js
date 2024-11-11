5ad8f9fc4c0ba45679b94f8e752dc08cb6d64393
matias <matias.calvo@outlook.com>
2024-11-11 18:46:18 -0300

    hecho ejercicio tres

--- m3-u4/main.js
+++ m3-u4/main.js
@@ -14,3 +14,63 @@ document.getElementById('text-area').addEventListener('input', function() {
     const largoTexto = this.value.length
     document.getElementById('contCar').textContent = largoTexto
 })
+
+var alumnos = [
+
+    {
+  
+      nombre: 'Juan Gomez',
+  
+      nota: 7
+  
+    }, {
+  
+      nombre: 'Pedro Rodriguez',
+  
+      nota: 4
+  
+    }, {
+  
+      nombre: 'Roxana García',
+  
+      nota: 8
+  
+    }, {
+  
+      nombre: 'Luciano Lopez',
+  
+      nota: 5
+  
+    }, {
+  
+      nombre: 'Fernanda Gimenez',
+  
+      nota: 6
+  
+    }, {
+  
+      nombre: 'Florencia Martinez',
+  
+      nota: 10
+  
+    }, {
+  
+      nombre: 'Raul Sanchez',
+  
+      nota: 7
+  
+    }, {
+  
+      nombre: 'Sandra Figueroa',
+  
+      nota: 8
+  
+    }
+  
+  ];
+
+  let alumnosAprobados = alumnos.filter(function(alumno){
+    return alumno.nota >= 7
+  })
+
+  console.log(alumnosAprobados)

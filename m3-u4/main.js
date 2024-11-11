9dd99d71b4a16ad10db623a587d7e91244e60712
matias <matias.calvo@outlook.com>
2024-11-11 15:54:15 -0300

    hecho ejercicio dos

--- m3-u4/main.js
+++ m3-u4/main.js
@@ -6,6 +6,11 @@ document.getElementById('cuadrado2').addEventListener('click', function() {
     this.style.backgroundColor = 'orange'
 })
 
-  document.getElementById('cuadrado3').addEventListener('click', function() {
+document.getElementById('cuadrado3').addEventListener('click', function() {
     this.style.backgroundColor = 'aquamarine'
+})
+
+document.getElementById('text-area').addEventListener('input', function() {
+    const largoTexto = this.value.length
+    document.getElementById('contCar').textContent = largoTexto
 })

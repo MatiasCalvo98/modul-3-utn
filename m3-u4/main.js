fcec9193db221a33a7324d28f5dd5e2e09940edc
matias <matias.calvo@outlook.com>
2024-11-11 11:56:11 -0300

    hecho ejercicio 1

---
+++ m3-u4/main.js
@@ -0,0 +1,11 @@
+document.getElementById('cuadrado1').addEventListener('click', function() {
+  this.style.backgroundColor = 'yellow'
+})
+
+document.getElementById('cuadrado2').addEventListener('click', function() {
+    this.style.backgroundColor = 'orange'
+})
+
+  document.getElementById('cuadrado3').addEventListener('click', function() {
+    this.style.backgroundColor = 'aquamarine'
+})
